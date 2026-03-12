372aed67583f9c10872279b821579f3372061fb9
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-12 16:45:45 +0700

    feat: finish the hero section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,19 +9,25 @@ export default function Home() {
     <>
       <section
         id="hero"
-        className="flex flex-col items-center justify-center text-center min-h-screen"
+        className="flex flex-col items-center justify-center
+         text-center min-h-screen tracking-widest gap-4 bg-cover opa bg-center bg-[url('/header_bg.png')] "
       >
-        <p className="uppercase">
+        
+        <p className="uppercase text-sm tracking-[0.35em]">
           Indie Game Developer
         </p>
 
-        <p className="uppercase text-primary text-3xl">
+        <p className="uppercase font-title font-bold text-7xl">
           {profile.main_quote}
         </p>
 
-        <p className="font-body text-lg text-textmaincolor mt-4">
+        <p className="font-body text-sm text-textmaincolor mt-4">
           {profile.sub_quote}
         </p>
+
+        <a href="#games" className="text-primary mt-8">
+          VIEW MY WORK
+        </a>
       </section>
 
       <section id="games" className="min-h-screen">
