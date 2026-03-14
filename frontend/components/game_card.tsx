2c34a74c4edc1b5613a039358eaf212f85e134a0
Ichi1234 <ichigamer111th@gmail.com>
2026-03-14 15:44:51 +0700

    chore: commit to use different pc

--- frontend/components/game_card.tsx
+++ frontend/components/game_card.tsx
@@ -6,8 +6,12 @@ type Props = {
 
 export default function GameCard({ game }: Props) {
   return (
-    <div>
-      <h3>{game.title}</h3>
+    <div className="relative bg-cardbackground p-4 w-90 h-50">
+      <div className="absolute top-4 left-4 p-2 inline border-2 border-primary bg-primary/20 text-primary uppercase">
+        { game.type }
+      </div>
+
+      <h2>{ game.title }</h2>
     </div>
   );
 }
