1fca3c869c0941b74d826fa05161d442a3461c32
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-19 23:17:59 +0700

    feat: job context page add form component still doesn't have label but I will do it tmr

--- frontend/app/job-contact/page.tsx
+++ frontend/app/job-contact/page.tsx
@@ -2,7 +2,15 @@
 
 
 export default function JobContact() {
+    
     return (
-        <p className="mt-100">test</p>
+        <main className="container max-w-4xl mx-auto mt-40">
+            <form className="flex flex-col gap-3">
+                <input name="name" placeholder="Collaboration Opportunity on Exciting Game Project" className="p-2 border-b border-primary" />
+                <textarea name="message" placeholder="Write your messages here..." className="px-5 py-6 bg-cardbackground border border-[#2b2826] rounded h-32" />
+                <button type="submit" className="self-start mt-4 px-6 py-2 bg-primary font-semibold text-black rounded-lg">SEND MESSAGE</button>
+            </form>
+
+        </main>
     );
 }
--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -10,9 +10,9 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-xl">
       <div className="p-5 sm:text-sm lg:text-lg flex flex-col gap-y-2 sm:gap-0 sm:flex-row justify-around items-center tracking-widest">
-        <a href="#hero" className="font-title text-primary uppercase">
+        <Link href="/#hero" className="font-title text-primary uppercase">
           { profile?.portfolio_title }
-        </a>
+        </Link>
 
         <div className="flex sm:block font-body text-[0.65rem] sm:text-xs lg:text-sm space-x-4 sm:space-x-8 text-textmaincolor">
           <Link href="/#games">GAMES</Link>
