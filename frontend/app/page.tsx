91b227186841e84728a08bb8cb079facd4fe56b8
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-19 17:07:10 +0700

    feat: finished contact section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -84,8 +84,59 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="contact" className="min-h-screen">
-        <h2 className="text-primary text-3xl">Contact</h2>
+      <section id="contact" className="relative py-28 flex flex-col gap-y-4 items-center">
+        <div className="absolute left-1/2 top-0 -translate-x-1/2 mb-4 w-px h-20 bg-linear-to-t from-primary/30 to-transparent"></div>
+        <p className="text-textsubcolor sm:text-xs xl:text-sm tracking-[0.35em]">GET IN TOUCH</p>
+
+        <h2 className="font-title font-bold text-4xl mb-4">Let's  <span className="text-primary">Collaborate</span></h2>
+
+        <p className="max-w-2xl text-center mb-8">
+          Interested in collaboration, internship opportunities, or discussing game systems and architecture? Feel free to reach out.
+        </p>
+
+        <div className="flex gap-x-4 text-sm">
+          <a className="text-primary bg-[#272321] flex justify-evenly items-center gap-2 border border-primary/40 rounded-lg px-8 py-2">
+            
+            {/* Mail Icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-mail">
+              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+            </svg>
+            
+            EMAIL ME
+
+            {/* Arrow Thingy */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" 
+            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+            className="lucide lucide-arrow-up-right transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5">
+              <path d="M7 7h10v10"></path>
+              <path d="M7 17 17 7"></path>
+            </svg>
+          </a>
+
+          <a className="bg-[#272321] flex justify-evenly items-center gap-2 border border-[#2b2826] rounded-lg px-8 py-3">
+            
+            {/* Github Icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+            fill="none" stroke="currentColor" 
+            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+            className="lucide lucide-github">
+              <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
+              <path d="M9 18c-4.51 2-5-2-7-2"></path>
+            </svg>
+            
+            GITHUB
+
+            {/* Arrow Thingy */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" 
+            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+            className="lucide lucide-arrow-up-right transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5">
+              <path d="M7 7h10v10"></path>
+              <path d="M7 17 17 7"></path>
+            </svg>
+          </a>
+        </div>
       </section>
     </>
   );
