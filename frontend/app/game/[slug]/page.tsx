4ccb1b55bd2e68b9cd87aa78a1e638a693fcf1d7
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-03-21 17:26:09 +0700

    feat: finished half of the game detail page

--- frontend/app/game/[slug]/page.tsx
+++ frontend/app/game/[slug]/page.tsx
@@ -6,11 +6,13 @@ import { useParams } from "next/navigation";
 import { useGameData } from "@/context/game_context";
 import { convertGameNameToId } from "@/utils/gamename_to_id";
 import Link from "next/link";
+import { formatDate } from "@/utils/format_date";
+import Chip from "@/components/chip";
 
 export default function GameDetail() {
     const params = useParams();
     const slug = params?.slug as string | undefined;
-
+    
     const games = useGameData();
 
     const game = games.find(
@@ -24,7 +26,7 @@ export default function GameDetail() {
     return (
         <div className="flex flex-col gap-y-4 min-h-screen">
 
-            <div className="fixed top-0 w-full h-80">
+            <div className="absolute top-0 w-full h-65">
                 <Image
                     className="object-cover "
                     src={game.cover_img_path || "/img/default_cover_img.png"}
@@ -36,9 +38,10 @@ export default function GameDetail() {
 
             </div>
             
-            <div className="z-10 px-4 lg:px-60 mt-30">
+            {/* Main Description */}
+            <div className="flex flex-col gap-y-2 z-10 px-4 lg:px-60 mt-40">
 
-                <div className="flex items-center gap-x-2 text-sm mb-8">
+                <div className="flex items-center gap-x-2 text-sm mb-2">
                     {/* ARROWWWW */}
                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" 
                         fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" 
@@ -47,18 +50,103 @@ export default function GameDetail() {
                         <path d="M19 12H5"></path>
                     </svg>
                     
-                    <Link href="/">Back to Games</Link>
+                    <Link href="/#games">Back to Games</Link>
                 </div>
                 
-                <div className="text-primary uppercase">
+                <div className="w-fit px-3 py-1 rounded-sm text-sm bg-black border border-primary/40 text-primary uppercase">
                     {game.type}
                 </div>
 
-                <h1 className="text-3xl font-title">{game.title}</h1>
+                <div>
+                    <h1 className="text-4xl mt-2 text-white font-bold font-title">{game.title}</h1>
 
+                    <div className="flex gap-4 items-center text-sm px-0.5">
+                        <span className="text-textsubcolor">{game.release_date != "" ? formatDate(game.release_date) : "In Progress"}</span>
+                        {game?.repository_link && (
+                            <a href={game.repository_link} target="_blank" rel="noopener noreferrer" className="flex items-center gap-x-1">
+                                <Image
+                                    className="mb-0.5"
+                                    src={"/img/github_icon.png"}
+                                    alt="GitHub Icon"
+                                    width={16}
+                                    height={16}
+                                />
+
+                                GitHub Repository
+
+                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none"
+                                    stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                    className="lucide lucide-arrow-up-right">
+                                    <path d="M7 7h10v10"></path>
+                                    <path d="M7 17 17 7"></path>
+                                </svg>
+                            </a>
+                        )}
+                    </div>
+                </div>
                 
+                <div className="flex flex-col gap-y-10 mt-12">
+                    <div className="flex gap-x-4">
+                        <button className="flex items-center gap-x-2 px-6 py-3 bg-primary text-black text-sm font-semibold rounded-lg">
 
-                <p className="mt-4">{game.description}</p>
+                            {/* Download Icon */}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                className="lucide lucide-download">
+                                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                <polyline points="7 10 12 15 17 10"></polyline>
+                                <line x1="12" x2="12" y1="15" y2="3"></line>
+                            </svg>
+
+                            DOWNLOAD
+                        </button>
+
+                        <button className="flex items-center gap-x-2 px-6 py-3 border border-primary/40 bg-[#272321] text-white text-sm font-semibold rounded-lg">
+
+                            {/* Plus Icon */}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                className="lucide lucide-plus">
+                                <path d="M5 12h14"></path>
+                                <path d="M12 5v14"></path>
+                            </svg>
+
+                            FOLLOW
+
+                        </button>
+                    </div>
+                        
+                    <div className="p-7 bg-cardbackground border border-[#332e2b] max-w-2xl rounded-xl">
+                        <h2 className="text-white font-title text-xl mb-4">Description</h2>
+                        <p>{game.description}</p>
+                    </div>
+                    
+                    <div className="flex gap-x-6">
+                        <div className="px-5 py-3 bg-cardbackground border border-[#332e2b] w-fit rounded-lg">
+                            <h2 className="text-textsubcolor font-title text-sm mb-2">Platforms</h2>
+                            
+                            <div className="flex gap-3">
+                              {game.platforms.map((platform) => (
+                                <Chip key={platform} label={platform} className="text-white"/>
+                              ))}
+                            </div>
+                           
+                        </div>
+
+                        <div className="px-5 py-3 bg-cardbackground border border-[#332e2b] w-fit rounded-lg">
+                            <h2 className="text-textsubcolor font-title text-sm mb-2">Tags</h2>
+                            
+                            <div className="flex gap-3">
+
+                              {game.tags.map((tag) => (
+                                <Chip key={tag} label={tag} className="text-white"/>
+                              ))}
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
             
         </div>
