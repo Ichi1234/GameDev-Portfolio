c2716057a9b76f19e7d0ebcee2ced48e9730061a
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-04-04 11:00:20 +0700

    chore: back to the game list btn in add form

--- frontend/components/admin_form/game/add_form.tsx
+++ frontend/components/admin_form/game/add_form.tsx
@@ -57,6 +57,27 @@ export default function GameAddForm({ setScreen }: Props) {
 
     return (
         <form className="flex flex-col gap-6 font-title">
+            <span onClick={() => setScreen("home")} className="mb-4 flex items-center gap-1 text-sm text-admintitle font-body font-semibold cursor-pointer">
+                {/* Arrow Left */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-arrow-left"
+                    >
+                    <path d="m12 19-7-7 7-7"></path>
+                    <path d="M19 12H5"></path>
+                </svg>
+
+                Back to the Game List
+            </span>
+
             <div>
                 <label className="text-admintitle">Title</label>
                 <input
