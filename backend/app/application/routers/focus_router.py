e4524288f03179b57be8659cf916b05fb1a22496
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-04-06 10:10:47 +0700

    feat: implement focus router

---
+++ backend/app/application/routers/focus_router.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from backend.app.data.database import get_db
+from backend.app.data.models.profile_model import OwnerFocus
+from backend.app.application.schemas.profile_schema import FocusCreate
+
+router = APIRouter(prefix="/focus", tags=["Focus"])
+
+
+@router.post("/")
+def create_focus(body: FocusCreate, db: Session = Depends(get_db)):
+    focus = OwnerFocus(
+        name = body.name
+    )
+
+    db.add(focus)
+    db.commit()
+    db.refresh(focus)
+
+    return {
+        "focus": focus.id,
+        "name" : focus.focus
+    }
+
+
+@router.delete("/")
+def delete_focus(remove_id: int, db: Session = Depends(get_db)):
+    focus = db.query(OwnerFocus).filter(OwnerFocus.id == remove_id).first()
+
+    if not focus:
+        return {"error": "Focus not found"}
+
+    response = {
+        "id": focus.id,
+        "name": focus.focus
+    }
+
+    db.delete(focus)
+    db.commit()
+
+    return response
