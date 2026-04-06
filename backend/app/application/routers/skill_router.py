d28e38028249987e79cf3393a8d05951635ad98c
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-04-06 10:15:05 +0700

    feat: create skill router

---
+++ backend/app/application/routers/skill_router.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from backend.app.data.database import get_db
+from backend.app.data.models.profile_model import OwnerSkill
+from backend.app.application.schemas.profile_schema import SkillCreate
+
+router = APIRouter(prefix="/skill", tags=["Skill"])
+
+
+@router.post("/")
+def create_focus(body: SkillCreate, db: Session = Depends(get_db)):
+    skill = OwnerSkill(
+        skill = body.name,
+        description = body.description
+    )
+
+    db.add(skill)
+    db.commit()
+    db.refresh(skill)
+
+    return {
+        "id": skill.id,
+        "name" : skill.skill,
+        "description" : skill.description
+    }
+
+
+@router.delete("/")
+def delete_focus(remove_id: int, db: Session = Depends(get_db)):
+    skill = db.query(OwnerSkill).filter(OwnerSkill.id == remove_id).first()
+
+    if not skill:
+        return {"error": "Skill not found"}
+
+    response = {
+        "id": skill.id,
+        "name": skill.focus
+    }
+
+    db.delete(skill)
+    db.commit()
+
+    return response
